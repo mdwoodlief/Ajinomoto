d444475a56080515f071f6f65b801839d95627df
Matthew Woodlief <mdwoodlief@gmail.com>
2024-07-17 20:01:04 -0400

    Refined ProjectCounter.py
    Added if statement to catch invalid choices by the user. Added more comments to highlight areas to change if more file types are introduced or file paths are changed.

--- ProjectCounter.py
+++ ProjectCounter.py
@@ -1,15 +1,30 @@
 import os
+import sys
+
+
 def main():
-    #user instructions
-    print("Enter a category of projects to summarize from the following: 'Ongoing', 'Completed', or 'Backlog'.")
-    #add try and except statements to catch errors
-    
-    #uniform input andto function
-    total(input("").strip().casefold())
+    # user instructions
+    print(
+        "Enter a category of projects to summarize from the following: 'Ongoing', 'Completed', or 'Backlog': "
+    )
+
+    # list of choices for catching incorrect inputs
+    choices = ["ongoing", "completed", "backlog"]
+
+    # uniform input
+
+    while True:
+        s = input("").strip().casefold()
+        if s in choices:
+            total(s)
+            break
+        else:
+            print("Invalid user entry. Try again: ")
 
 
 def total(s):
-    #define variables for counting project types
+    # define variables for counting project types
+    # add or remove variables as needed
     Ad = 0
     NP = 0
     O = 0
@@ -17,16 +32,23 @@ def total(s):
     WP = 0
     U = 0
 
-    #identifiy correct path
-    #path = os.getcwd()
+    # identifiy correct path
+    # update paths as necessary
     if s == "ongoing":
-        file_list = os.listdir("R:\Quality\Quality_Control\QC Scientist\Projects_Ongoing")
-    elif s =="backlog":
-        file_list = os.listdir("R:\Quality\Quality_Control\QC Scientist\Project_Backlog")
-    elif s =="completed":
-        file_list = os.listdir("R:\Quality\Quality_Control\QC Scientist\Archived\Projects_Completed")
+        file_list = os.listdir(
+            "R:\Quality\Quality_Control\QC Scientist\Projects_Ongoing"
+        )
+    elif s == "backlog":
+        file_list = os.listdir(
+            "R:\Quality\Quality_Control\QC Scientist\Project_Backlog"
+        )
+    elif s == "completed":
+        file_list = os.listdir(
+            "R:\Quality\Quality_Control\QC Scientist\Archived\Projects_Completed"
+        )
 
-    #iterate over list to add to categories
+    # iterate over list to add to categories
+    # add or remove categories to match variables as needed
     for file in file_list:
         if file.count("Addendum") > 0:
             Ad += int(file.count("Addendum"))
@@ -40,7 +62,10 @@ def total(s):
             WP += int(file.count("White Paper"))
         else:
             U += 1
-    print(f"Addendum {Ad}, New Product {NP}, Other {O}, Validation {Val}, White Paper {WP}, Unknown {U}")
+    print(
+        f"Addendum {Ad}, New Product {NP}, Other {O}, Validation {Val}, White Paper {WP}, Unknown {U}"
+    )
+
 
 if __name__ == "__main__":
     main()
